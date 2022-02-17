8706d85617b9b1dba60b478f9fb3c5a5118157aa
ldf <690115491@qq.com>
2022-02-17 16:51:57 +0800

    feat(): remove redirect;

--- next.config.js
+++ next.config.js
@@ -18,15 +18,6 @@ const nextConfig = {
 
     return config;
   },
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/",
-        permanent: true,
-      },
-    ];
-  },
 };
 
 module.exports = withTM(withImages(nextConfig));
